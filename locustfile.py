e361a1600e3bad84305a563ea7c7c9e5429cc0d1
VarunRustagi <varunrustagi@shorthills.ai>
2025-03-21 16:10:22 +0530

    locustfile changes

--- locustfile.py
+++ locustfile.py
@@ -8,5 +8,4 @@ class MyUser(HttpUser):
     def infer(self):
         with self.client.post("/inference", json={"test": "readmission", "data": "tokenized_datasets/mimic_test_timelines_p10.hdf5", "vocab": "tokenized_datasets/mimic_vocab_t4367.pkl", "model": "/dbfs/FileStore/tables/mnt/models/best_model.pt", "output": "/dbfs/FileStore/tables/mnt/results/readmission",  "mode": "profile", "n_jobs": 8, "n_gpus": 1}, catch_response=True) as response:
             latency = response.elapsed.total_seconds()
-            throughput = 1 / latency
-            print(f"Latency: {latency}s, Throughput: {throughput} req/s")
+            print(f"Latency: {latency}s")
